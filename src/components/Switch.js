c2b680053827f0de06113b46bd8b2d7f28c921ba
Stéphane Dauguet <sdauguet@gmail.com>
2021-04-28 17:11:58 +0200

    issue corrected

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -2,13 +2,16 @@ const Switch = ({ text, state, sw, setSwitch }) => {
   // state : button active or inactive
   // sw : value du switch
   const handleClick = () => {
-    console.log(sw);
     setSwitch(!sw);
   };
   return (
     <div
       className={`switch ${state ? "active" : "inactive"}`}
-      onClick={!state && handleClick} // onClick only if button is inactive
+      onClick={() => {
+        if (!state) {
+          handleClick();
+        }
+      }}
     >
       <span>{text}</span>
     </div>
